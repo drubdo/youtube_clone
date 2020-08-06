2ae5c717a331a98441f060a6c247a7d67664001f
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-06 18:23:23 -0500

    added axios package

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Axios from 'axios'
+import config from 'config';
 
 class Search extends React.Component { 
+    search = () => { 
+        Axios.get(`https://www.googleapis.com/youtube/v3/search?q=cars&key=${config.youtubeApi}`).then(res => {
+            console.log(res)
+        })
+    } 
     render(){
         return (
-            <div>Test</div>
+            <div>
+                <input type="text" id="fname" name="fname"/> <button type="button" onClick={this.search}>Click Me!</button>
+            </div>
         )
     }
 }
