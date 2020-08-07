554618a06c02971a87802a0726367fbf9a198a1c
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-06 20:59:48 -0500

    added search class, added onClick function to search youtube API & added iframe tag to view video

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,17 +1,46 @@
 import React from 'react'
-import Axios from 'axios'
-import config from 'config';
+import Axios from 'axios';
+
 
 class Search extends React.Component { 
+    constructor(props){
+        super(props)
+        this.config = {
+            youtubeApi: ''
+        }
+        this.state = {
+            query : "", 
+            currentVideo : ""
+        }
+    
+    }   
+    componentDidMount(){
+        
+    } 
+    
     search = () => { 
-        Axios.get(`https://www.googleapis.com/youtube/v3/search?q=cars&key=${config.youtubeApi}`).then(res => {
+        Axios.get(`https://www.googleapis.com/youtube/v3/search?type=video&autoplay=1&q=${this.state.query}&key=${this.config.youtubeApi}`).then(res => {
             console.log(res)
+            console.log(res.data.items[0].id.videoId)
+            this.setState({
+                currentVideo: 'https://www.youtube.com/embed/' + res.data.items[0].id.videoId + '?autoplay=1'
+            })
+            console.log('https://www.youtube.com/watch?v=' + res.data.items[0].id.videoId)
+
         })
     } 
+    queryHandler = (e) => {
+        this.setState({
+            query: e.target.value
+        })
+    }
+
     render(){
         return (
             <div>
-                <input type="text" id="fname" name="fname"/> <button type="button" onClick={this.search}>Click Me!</button>
+                <input type="text" id="fname" name="fname" onChange={this.queryHandler}/> <button type="button" onClick={this.search}>Click Me!</button>
+                <iframe allow="autoPlay" width="420" height="315" src={this.state.currentVideo} title="videos"></iframe>
+            
             </div>
         )
     }
