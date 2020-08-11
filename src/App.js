530f22abe5b9b3f6ef154d2f9a883199a6402750
Ritz Dacanay <rdacanay@Eye-Fi.local>
2020-08-11 16:05:03 -0700

    Create header function component which will display the name of the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Search from './components/search/search'; 
+import Header from './components/header'; 
 
 function App() {
   return (
     <div className="App">
+      <Header name="Youtube Api Clone"/>
       <Search/>
     </div>
   );
