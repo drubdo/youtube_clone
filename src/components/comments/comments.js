f954f5ff2e82a4ec869509b594629c514845f8e4
Ritz Dacanay <rdacanay@Eye-Fi.local>
2020-08-13 15:54:24 -0700

    styled comment input field

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import CommentDetails from './commentDetails';
+import { Container, Row, Col, FormGroup, Label, Input, Button } from 'reactstrap';
 
 class Comments extends Component {
 
@@ -17,15 +18,26 @@ class Comments extends Component {
     }
 
     addNewComment = () => {
-        this.props.addNewComment(this.state.newComment)
+        this.props.addNewComment(this.state.newComment);
+        this.setState({
+            "newComment": ""
+        });
     }
 
     render() {
         return (
             <div>
-                <h3>Comment Section {this.props.videoId}</h3>
-                Add comment: <input type="text" onChange={this.commentOnHandler} />
-                <button onClick={this.addNewComment}>Add comment</button>
+                <Container className="themed-container">
+                    <Row style={{ padding: "15px" }}>
+                        <Col sm="12" md={{ size: 6, offset: 3 }}>
+                            <FormGroup>
+                                <Label for="exampleText">Add a comment</Label>
+                                <Input type="textarea" name="text" id="exampleText" placeholder="Add a comment" onChange={this.commentOnHandler} />
+                            </FormGroup>
+                            <Button outline color="success" onClick={this.addNewComment}>Comment</Button>
+                        </Col>
+                    </Row>
+                </Container>
                 <CommentDetails videoId={this.props.videoId} commentDetails={this.props.commentDetails} likeOnHandler={this.props.likeOnHandler} dislikeOnHandler={this.props.dislikeOnHandler} submitReply={this.props.submitReply} />
             </div>
         )
