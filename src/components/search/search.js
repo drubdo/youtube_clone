9a48032a1a0abaa71e457880755f5077375be62b
david rubio <rubdo@davids-Air.attlocal.net>
2020-08-12 19:39:23 -0500

    added enter on click feature

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,86 +2,94 @@ import React from 'react'
 import Axios from 'axios';
 import Test from '../relatedVideos/relatedVideos';
 
-class Search extends React.Component { 
-    constructor(props){
+class Search extends React.Component {
+    constructor(props) {
         super(props)
         this.config = {
             youtubeApi: 'AIzaSyBMpy7LKrgHmjC8PyzIgjtTYDa0K07Uq-U'
         }
         this.state = {
-            query : "", 
-            currentVideo : "",
-            videoId : "",
-            relatedVideos : []
+            query: "",
+            currentVideo: "",
+            videoId: "",
+            relatedVideos: []
         }
-    
-    }   
-    componentDidMount(){
-        
-    } 
-    
-    search = () => { 
+
+    }
+    componentDidMount() {
+
+    }
+
+    search = () => {
+        alert("hit me")
         Axios.get(`https://www.googleapis.com/youtube/v3/search?type=video&autoplay=1&q=${this.state.query}&key=${this.config.youtubeApi}`).then(res => {
             console.log(res)
             console.log(res.data.items[0].id.videoId)
             this.setState({
                 currentVideo: 'https://www.youtube.com/embed/' + res.data.items[0].id.videoId + '?autoplay=1',
-                videoId : res.data.items[0].id.videoId
+                videoId: res.data.items[0].id.videoId
             })
             this.RelatedVideos();
             console.log('https://www.youtube.com/watch?v=' + res.data.items[0].id.videoId)
 
         })
-    } 
+    }
     queryHandler = (e) => {
         this.setState({
             query: e.target.value
         })
     }
 
-    RelatedVideos = () => { 
+    RelatedVideos = () => {
         Axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${this.state.videoId}&type=video&maxResults=10&key=${this.config.youtubeApi}`).then(res => {
             console.log('relatedVideos', res)
             this.setState({
-                relatedVideos:res.data.items
+                relatedVideos: res.data.items
             })
-            
 
-        })
-    } 
- 
-    playVideo = (video) => {
-        this.setState({
-            currentVideo: 'https://www.youtube.com/embed/' + video.id.videoId + '?autoplay=1',
-            videoId : video.id.videoId
+
         })
     }
 
-    render(){
+    playVideo = (video) => {
+        this.setState({
+            currentVideo: 'https://www.youtube.com/embed/' + video.id.videoId + '?autoplay=1',
+            videoId: video.id.videoId
+        })
+    }
+
+    render() {
         return (
-            
+
             <div>
 
-                <div style={{width:"100%",backgroundColor:"orange", height:"250px",overflowX:'scroll', whiteSpace:'nowrap', padding:'20px'}}>
-                related videos
+                <div style={{ width: "100%", backgroundColor: "orange", height: "250px", overflowX: 'scroll', whiteSpace: 'nowrap', padding: '20px' }}>
+                    related videos
                 {
-                    this.state.relatedVideos.map((video, index) =>
-                        <div style={{display:'inline-block'}}>
-                            
-                            <iframe allow="autoPlay" width="300" height="180" src={"https://www.youtube.com/embed/" + video.id.videoId} title="videos"></iframe>
-                            <button onClick={() => this.playVideo(video)}>Play</button>
-                        </div>
-                    )
-                }
-            </div>
-            <br/> 
+                        this.state.relatedVideos.map((video, index) =>
+                            <div style={{ display: 'inline-block' }}>
 
-                <input type="text" id="fname" name="fname" onChange={this.queryHandler}/> <button type="button" onClick={this.search}>Click Me!</button>
-                <br/> <iframe allow="autoPlay" width="420" height="315" src={this.state.currentVideo} title="videos"></iframe>
-            
+                                <iframe allow="autoPlay" width="300" height="180" src={"https://www.youtube.com/embed/" + video.id.videoId} title="videos"></iframe>
+                                <button onClick={() => this.playVideo(video)}>Play</button>
+                            </div>
+                        )
+                    }
+                </div>
+                <br />
+
+                <form onSubmit={this.search} >
+                    <input type="text" id="fname" name="fname" onChange={this.queryHandler}/>
+
+                    <div class="btn-group" >
+                        <button type="submit">onSubmit</button>
+                    </div>
+
+                </form> 
+
             </div>
         )
     }
 }
-export default Search; 
+export default Search;
+
 
